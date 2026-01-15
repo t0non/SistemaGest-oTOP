8ecf0c983dee2bf181a7ae8483d6195861a5ae76
Eduardo Tonon <eduardosoarestonon@gmail.com>
2026-01-15 13:35:34 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4772271246-e1289",
+  "appId": "1:332630561458:web:dd5aa4a48cb013171defab",
+  "apiKey": "AIzaSyAbhgQPmfkl6MOBMJRHdXH4i34ib991Tbw",
+  "authDomain": "studio-4772271246-e1289.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "332630561458"
+};
